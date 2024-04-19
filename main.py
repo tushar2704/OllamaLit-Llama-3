0103a948a521569cadf6a30e44e62224953991a9
tushar27 <tushar.27041994@gmail.com>
2024-04-19 23:00:23 +0530

    structure

---
+++ main.py
@@ -0,0 +1,7 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##OllamaLit [https://github.com/tushar2704/OllamaLit] (https://github.com/tushar2704/OllamaLit)
+#######################################################################################################
+#Importing dependecies
+#######################################################################################################
+import os
+import sys
