8d272140c23dfd4b8bb1c5a94d2f3b0d85d4bc62
tushar27 <tushar.27041994@gmail.com>
2024-04-21 16:38:23 +0200

    pages

---
+++ pages/2_Settings.py
@@ -0,0 +1,15 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##OllamaLit [https://github.com/tushar2704/OllamaLit] (https://github.com/tushar2704/OllamaLit)
+#######################################################################################################
+#Importing dependecies
+#######################################################################################################
+import os
+import sys
+import ollama
+import streamlit as st
+from openai import OpenAI
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+#######################################################################################################
